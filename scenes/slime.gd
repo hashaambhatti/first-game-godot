6ea3749839bd2d241da5a90a67ceb25d5139baa9
Hashaam Bhatti <142686798+hashaambhatti@users.noreply.github.com>
2024-07-01 12:37:04 +0500

    Added enemy (3) and modified dying.
    make dying look alot better than before, updated the script of enemy

--- scenes/slime.gd
+++ scenes/slime.gd
@@ -1,20 +1,25 @@
 extends Node2D
 
-const SPEED = 50
+const SPEED = 60
+
 var direction = 1
 
 @onready var rc_left = $RCLeft
 @onready var rc_right = $RCRight
 @onready var slime = $AnimatedSprite2D
+@onready var rc_down = $RCDown
+@onready var rc_up = $RCUp
 
 func _process(delta):
 	
 	if rc_right.is_colliding():
 		direction = -1
-		slime.flip_h = true
+		slime.flip_h = false
+		
 	if rc_left.is_colliding():
 		direction = 1
-		slime.flip_h = false
-	
-	position.x = SPEED * delta * direction
+		slime.flip_h = true
+		
+	print("Enemy is moving")
+	position.x += SPEED * delta * direction
 	
